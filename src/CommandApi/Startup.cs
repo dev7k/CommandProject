6cb4d495e53b578a1996c1fd62242dc85463e0ad
dev7k <dev7k@protonmail.com>
2020-07-17 22:18:46 +0200

    added commands controller with configuration in startup class

--- src/CommandApi/Startup.cs
+++ src/CommandApi/Startup.cs
@@ -16,7 +16,7 @@ namespace CommandApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,10 +31,7 @@ namespace CommandApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
