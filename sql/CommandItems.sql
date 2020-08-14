2607c1296a1c239fb1f6adba76702b474de7a91f
dev7k <dev7k@protonmail.com>
2020-08-14 23:18:45 +0200

    added sql queries

---
+++ sql/CommandItems.sql
@@ -0,0 +1,13 @@
+select * from "CommandItems"
+
+insert into "CommandItems" ("HowTo", "Platform", "CommandLine")
+values ('Create an EF migration', 'Entity Framework Core Command Line', 'dotnet ef migrations add');
+
+insert into "CommandItems" ("HowTo", "Platform", "CommandLine")
+values ('Apply Migrations to DB', 'Entity Framework Core Command Line', 'dotnet ef database update');
+
+insert into "CommandItems" ("HowTo", "Platform", "CommandLine")
+values ('Create an EF Migration', 'Entity Framework Package Manager Console', 'add-migration <name of migration>')
+
+insert into "CommandItems" ("HowTo", "Platform", "CommandLine")
+values ('Apply Migrations to DB', 'Entity Framework Package Manager Console', 'update database')
